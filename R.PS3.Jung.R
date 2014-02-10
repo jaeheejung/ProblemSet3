f8a39ba01b803b8148c9ef45e5c50b1c9af65f3e
jaeheejung <jaeheejung@wustl.edu>
2014-02-10 00:52:42 -0600

    Problem Set 3
    Codes for problem Set 3

--- R.PS3.Jung.R
+++ R.PS3.Jung.R
@@ -7,6 +7,7 @@ library(foreach)
 library(doMC)
 library(multicore)
 
+
 ####SAMPLING DISTRIBUTIONS AND P-VALUES####
 ##1
 random.data <- rnorm(10000) 
@@ -72,6 +73,7 @@ registerDoMC(cores=4)
 #Registers parallel backend
 system.time(laply(.data=1:6,.fun=significant.ts,.parallel=TRUE))
 #In parallel, system time is significantly larger for all three time measures. An instance is user:0.041, system:0.080, elapsed:0.064. I tried running system.time with different numbers of cores, but system time only gets larger as the number of cores increases. In essence, no time is saved.
+
 
 ####CALCULATING FIT STATISTICS####
 ##1
@@ -155,8 +157,95 @@ return(cbind(rmse.vec,mad.vec,rmsle.vec,mape.vec,meape.vec,mrae.vec))
 }
 
 #3
+fit.stats.matrix2 <- function(y,P,r,RMSE,MAD,RMSLE,MAPE,MEAPE,MRAE){
+#This function creates a matrix of fit statistics for the three models I constructed before. Which statistics to compute can be specified by assigning TRUE to the desired statistic name arguments.
+rmse <- function(P){
+	sqrt(sum((P-y)^2,na.rm=TRUE)/length(y))
+}
+#rmse() calculates RMSE
+if(RMSE==TRUE){
+	rmse.vec <- aaply(.data=P,.margins=2,.fun=rmse)
+}else{rmse.vec <- NULL}
+#If RMSE equals TRUE, a vector of the values of RMSE for the three models is obtained by using aaply() across the columns of the prediction matrix. Otherwise, rmse.vec is NULL.
+mad <- function(P){
+	median(abs(P-y),na.rm=TRUE)
+}
+#mad() calculates MAD
+if(MAD==TRUE){
+	mad.vec <- aaply(.data=P,.margins=2,.fun=mad)
+}else{mad.vec <- NULL}
+#If MAD equals TRUE, a vector of the values of MAD for the three models is obtained by using aaply() across the columns of the prediction matrix. Otherwise, mad.vec is NULL.
+rmsle <- function(P){
+	sqrt(sum((log(P+1)-log(y+1))^2,na.rm=TRUE)/length(y))
+}
+#rmsle() calculates RMSLE
+if(RMSLE==TRUE){
+	rmsle.vec <- aaply(.data=P,.margins=2,.fun=rmsle)
+}else{rmsle.vec <- NULL}
+#If RMSLE equals TRUE, a vector of the values of RMSLE for the three models is obtained by using aaply() across the columns of the prediction matrix. Otherwise, rmsle.vec is NULL.
+mape <- function(P){
+	sum((abs(P-y)/abs(y))*100,na.rm=TRUE)/length(y)
+}
+#mape() calculates MAPE
+if(MAPE==TRUE){
+	mape.vec <- aaply(.data=P,.margins=2,.fun=mape)
+}else{mape.vec <- NULL}
+#If MAPE equals TRUE, a vector of the values of MAPE for the three models is obtained by using aaply() across the columns of the prediction matrix. Otherwise, mape.vec is NULL.
+meape <- function(P){
+	median((abs(P-y)/abs(y))*100,na.rm=TRUE)
+}
+#meape() calculates MEAPE
+if(MEAPE==TRUE){
+	meape.vec <- aaply(.data=P,.margins=2,.fun=meape)
+}else{meape.vec <- NULL}
+#If MEAPE equals TRUE, a vector of the values of MEAPE for the three models is obtained by using aaply() across the columns of the prediction matrix. Otherwise, meape.vec is NULL.
+mrae <- function(P){
+	median(abs(P-y)/abs(r-y),na.rm=TRUE)
+}
+#mrae() calculates MRAE
+if(MRAE==TRUE){
+	mrae.vec <-aaply(.data=P,.margins=2,.fun=mrae)
+}else{mrae.vec <- NULL}
+#If MRAE equals TRUE, a vector of the values of MRAE for the three models is obtained by using aaply() across the columns of the prediction matrix. Otherwise, mrae.vec is NULL.
+return(cbind(rmse.vec,mad.vec,rmsle.vec,mape.vec,meape.vec,mrae.vec))
+#The returning matrix contains vectors of the fit statistics that have been identified in the function argument. Since fit statistics that are assigned FALSE are NULL, they are not included in the matrix.
+}
 
+#The function below is exactly the same as the function above, except for the fact that all arguments related to MRAE are missing. Nonetheless, the function works when it's run.
+fit.stats.matrix3 <- function(y,P,r,RMSE,MAD,RMSLE,MAPE,MEAPE){
+rmse <- function(P){
+	sqrt(sum((P-y)^2,na.rm=TRUE)/length(y))
+}
+if(RMSE==TRUE){
+	rmse.vec <- aaply(.data=P,.margins=2,.fun=rmse)
+}else{rmse.vec <- NULL}
+mad <- function(P){
+	median(abs(P-y),na.rm=TRUE)
+}
+if(MAD==TRUE){
+	mad.vec <- aaply(.data=P,.margins=2,.fun=mad)
+}else{mad.vec <- NULL}
+rmsle <- function(P){
+	sqrt(sum((log(P+1)-log(y+1))^2,na.rm=TRUE)/length(y))
+}
+if(RMSLE==TRUE){
+	rmsle.vec <- aaply(.data=P,.margins=2,.fun=rmsle)
+}else{rmsle.vec <- NULL}
+mape <- function(P){
+	sum((abs(P-y)/abs(y))*100,na.rm=TRUE)/length(y)
+}
+if(MAPE==TRUE){
+	mape.vec <- aaply(.data=P,.margins=2,.fun=mape)
+}else{mape.vec <- NULL}
+meape <- function(P){
+	median((abs(P-y)/abs(y))*100,na.rm=TRUE)
+}
+if(MEAPE==TRUE){
+	meape.vec <- aaply(.data=P,.margins=2,.fun=meape)
+}else{meape.vec <- NULL}
+return(cbind(rmse.vec,mad.vec,rmsle.vec,mape.vec,meape.vec))
+}
 
-
-
-
+#4
+fit.stats.matrix(y,P,r)
+#Returns the matrix of the values of the six fit statistics. We can see from the result that across all statistics, the first model, which was model.1 <- lm(voteshare~chalspend+incspend,data=training.data), has the smallest error. This might suggest that explanatory variables related to incumbent and challenger spending are better in predicting incumbent vote share than are explanatory variables related to the incumbent's connection with the president, the electoral cycle, the incumbent's individual character, and the characteristic of the voting population.
